14866b37e1096c7e9acbfe8466d72f45c8fa5854
Albina <alba-madona@mail.ru>
2025-09-10 15:02:02 +0300

    commit

---
+++ src/main/java/org/example/Sum.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import lombok.Data;
+
+
+@Data
+public class Sum
+{
+   private int a;
+    private int b;
+
+    public Sum(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public static void main(String[] args) {
+        Sum sum1 = new Sum(6,2);
+        int result = sum1.a + sum1.b  ;
+        System.out.println("Result "  + result);
+
+    }
+}
